2a5b150503c72dd8eba4d6eedadb88696435c942
Iuri Guilherme Bogar Portilho <79106998+ibogar@users.noreply.github.com>
2026-04-17 09:03:46 -0300

    Update product_viewset.py
    add auth

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
 from product.serializers import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
+        
     queryset = Product.objects.all()
